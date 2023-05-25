0fb8ba10af758261645eb2802b27ed841c33728c
Happiness000 <133518972+Happiness000@users.noreply.github.com>
2023-05-25 11:06:49 +0100

    Create Script.php

---
+++ Script.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = "Sunday Nwanneka Happiness";
+$regnumber = "2021/HND/32827/CS";
+$soo = "Ebonyi state";
+$topic_of_project = "Web based biometric enable class attendance system using Finger print";
+
+
+echo "Name: $name<br>";
+echo "Regnumber: $regnumber<br>";
+echo "State of origin: $soo<br>";
+echo "Project topic: $topic_of_project<br>";
+
+?>
